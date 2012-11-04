90b81793dc994652cdc1d4313b715cf561583722
y-tsutsumi <tsutsu0928@gmail.com>
2012-11-04 14:20:22 +0900

    新規作成．

---
+++ TestProject/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonHelloWord = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonHelloWord
+            // 
+            this.buttonHelloWord.Location = new System.Drawing.Point(197, 227);
+            this.buttonHelloWord.Name = "buttonHelloWord";
+            this.buttonHelloWord.Size = new System.Drawing.Size(75, 23);
+            this.buttonHelloWord.TabIndex = 0;
+            this.buttonHelloWord.Text = "Hello";
+            this.buttonHelloWord.UseVisualStyleBackColor = true;
+            this.buttonHelloWord.Click += new System.EventHandler(this.buttonHelloWord_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.buttonHelloWord);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonHelloWord;
+    }
+}
+
