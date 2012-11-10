684c59626d52a37308487ed46c32f436af5c34dc
uemura <nt.uemura.shihomi@gmail.com>
2012-11-10 16:30:19 +0900

    ラジオボタン追加。

--- TestProject/Form1.Designer.cs
+++ TestProject/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.buttonHelloWord = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // buttonHelloWord
@@ -41,21 +42,35 @@
             this.buttonHelloWord.UseVisualStyleBackColor = true;
             this.buttonHelloWord.Click += new System.EventHandler(this.buttonHelloWord_Click);
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(30, 39);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(88, 16);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.buttonHelloWord);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button buttonHelloWord;
+        private System.Windows.Forms.RadioButton radioButton1;
     }
 }
 
